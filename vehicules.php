a9d6cd2406867aa58ece4054b33027c83a59c7ea
allan <decauxallan60@gmail.com>
2024-06-05 16:48:52 +0200

    ajout all

---
+++ vehicules.php
@@ -0,0 +1,75 @@
+<?php
+
+$vehicules = [
+    1=> $alfa = [
+        "nom"=> "Alfa Roméo",
+        1=> $mod1 = [
+            "url_photo"=> "///",
+            "nom"=> "modele 1",
+            "annee_debut"=> 1970,
+            "annee_fin"=> 1982,
+            "nbrProduit"=> 10,
+            "puissance_max"=> 2,
+            "puissance_min"=> 1,
+            "prix_neuf"=> 0,
+            "prix_actuel"=> 0,
+            "description"=> "Lorem...",
+        ],
+
+
+
+    ],
+    2=> $audi = [
+        "nom"=> "Audi",
+        1=> $mod1 = [
+            "url_photo"=> "///",
+            "nom"=> "modele 1",
+            "annee_debut"=> 1970,
+            "annee_fin"=> 1982,
+            "nbrProduit"=> 10,
+            "puissance_max"=> 2,
+            "puissance_min"=> 1,
+            "prix_neuf"=> 0,
+            "prix_actuel"=> 0,
+            "description"=> "Lorem...",
+        ],
+
+
+
+    ],
+    3=> $bmw = [
+        "nom"=> "BMW",
+        1=> $mod1 = [
+            "url_photo"=> "///",
+            "nom"=> "modele 1",
+            "annee_debut"=> 1970,
+            "annee_fin"=> 1982,
+            "nbrProduit"=> 10,
+            "puissance_max"=> 2,
+            "puissance_min"=> 1,
+            "prix_neuf"=> 0,
+            "prix_actuel"=> 0,
+            "description"=> "Lorem...",
+        ],
+
+
+
+    ],
+
+
+
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
