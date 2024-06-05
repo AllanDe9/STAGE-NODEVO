4e86c978a2d488642ab48401bfec53388ca846ab
allan <decauxallan60@gmail.com>
2024-06-05 16:46:01 +0200

    first commit

---
+++ accueil.php
@@ -0,0 +1,44 @@
+<?php
+$json_data = file_get_contents('data.json');
+$vehicules = json_decode($json_data, true);
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vintage Cars</title>
+    <link rel="stylesheet"  href="style/accueil.css">
+</head>
+<body>
+    <header>
+        <div class="container">
+            <a href="accueil.php">
+                <h1>Vintage Cars</h1>
+                <img src="image/car.jpg">
+            </a>
+            <nav>
+                <ul>
+                    <li><a href="#">Tous les modèles</a></li>
+                    <li>
+                        <a id="bouton" class="bouton" tabindex="0">Marques</a>
+                        <div class="menu" id="menu">
+                          <?php 
+                            foreach ($vehicules['marques'] as $marque) {
+                                    echo '<a href="marque.php?marque='.$marque['num_marque'].'">'.$marque['nom_marque'].'</a>';
+                            }
+                          ?>
+                        </div>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+    <main>
+
+
+
+
+    </main>
+</body>
+</html>
